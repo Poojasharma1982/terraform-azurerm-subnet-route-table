ea4beb693993986c95607d8b5434eee7908ae5a6
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-05 14:12:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+resource "azurerm_route_table" "myroutetable" {
+  name                = "routetable-name"
+  location            = "eastUS2"
+  resource_group_name = azurerm_resource_group.example.name
 
+  route {
+    name                   = "example"
+    address_prefix         = "10.100.0.0/14"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = "10.10.1.1"
+  }
+}
