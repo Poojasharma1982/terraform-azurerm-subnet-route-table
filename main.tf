9df3a77674b8a3b434cf7325668133ee2019112c
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-05 14:59:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_route_table" "myroutetable1" {
   resource_group_name = var.routetable-rg-name
 
   route {
-    name                   = "example"
+    name                   = "route1"
     address_prefix         = "10.100.0.0/14"
     next_hop_type          = "VirtualAppliance"
     next_hop_in_ip_address = "10.10.1.1"
