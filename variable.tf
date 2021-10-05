1fe54de6ad08ce36f6bd25b6e4fa9231c340eaca
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-05 14:18:56 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,12 @@
-
+variable "routetable-name" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
+variable "routetable-location" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
+variable "routetable-rg-name" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
